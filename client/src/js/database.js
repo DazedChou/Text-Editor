3f5ded6e190a74337fac59be8cf22a7820825392
David Chou <davidchou2009@gmail.com>
2022-02-27 20:24:42 -0800

    add logit to putDb functino

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,19 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate',1);
+  const tx = jateDb.transactions('jate','readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({todo: content});
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+  console.error('getDb not implemented');
+}
 
 initdb();
