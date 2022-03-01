85b9c15634a9cd5647be4b7f48f6d462ce3cabe2
David Chou <davidchou2009@gmail.com>
2022-02-28 22:31:37 -0800

    fix syntax typo in database.js, and added a return statement in method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,22 +15,23 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDb = await openDB('jate',1);
-  const tx = jateDb.transactions('jate','readwrite');
+  const tx = jateDb.transaction('jate','readwrite');
   const store = tx.objectStore('jate');
   const request = store.add({todo: content});
   const result = await request;
   console.log('🚀 - data saved to the database', result);
+  return result;
 }
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const jateDb = await openDB('jate',1);
-  const tx = jateDb.transactions('jate','readonly');
+  const tx = jateDb.transaction('jate','readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.value;
 
 }
 
